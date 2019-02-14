7a5f1e1f48bcd5bc9390243445235876a5069d7e
chengjiajian <378218354@qq.com>
2019-02-14 15:00:12 +0800

    insurance delete

--- insurance/cpic_package/test.py
+++
@@ -1,8 +0,0 @@
-import re
-
-
-message = r'地址:abcdffffaa哎哎哎111-..-__-'
-#message = re.sub('：',':',message)
-print(message)
-reg = re.compile('姓名:(.*?)')
-print(re.findall(reg,message))
--- insurance/珠峰/crews.py
+++
@@ -1,4 +0,0 @@
-from 保险接口.珠峰.inquiry import *
-import requests
-
-print(rewCar(vehiclePlateNo= '川YV1157',vehicleEnginNo='1625198190',vehicleVinNo='LGWEF4A58GF482916'))
